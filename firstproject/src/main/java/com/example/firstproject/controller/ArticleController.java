989a7437b2f18c18b4666deddcfaa4780764efb4
joyyKwon <1313439@naver.com>
2025-07-25 18:02:02 +0900

    add crud

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -38,7 +42,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
 //        System.out.println(saved.toString());
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles/{id}")
@@ -51,5 +55,56 @@ public class ArticleController {
         // 3. 뷰페이지 반환
 
         return "/articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. DB에서 모든 Article 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 가져온 Article 묶음 모델에 등록
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 사용자에게 보여줄 뷰 페이지 설정
+        return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 1. Db에서 Article 데이터 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 Article 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 반환
+        return "/articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        // 1. 폼을 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        // 2. DB 업데이트
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+        // 3. 뷰 반환
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        // 1. 데이터 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 2. 삭제
+        if (target != null) {
+            articleRepository.delete(target);
+            // 휘발성 데이터를 넘겨줄 수 있음
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+
+        // 3. 리다이렉션
+        return "redirect:/articles";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,6 +3,15 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     // Crud repo 상속으로 추가 구현없이 상속받은 애들 사용하면 됨.
+
+    // 필요하면 오버라이딩
+
+    @Override
+    ArrayList<Article> findAll();
+
+    void removeById(Long id);
 }
