370498e64c80c6c9b997d9df1fdd888045d252bb
joyyKwon <1313439@naver.com>
2025-07-28 15:35:34 +0900

    add service, test

---
+++ firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,118 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleService articleService;
+
+    /* Service 사용한 REST API 구현 */
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleService.index();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleService.show(id);
+    }
+
+    // POST
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+        Article created = articleService.create(dto);
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
+        List<Article> articleList = articleService.createArticles(dtos);
+        return (articleList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(articleList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+
+//    // Service 사용하지 않은 REST API 구현
+//    @Autowired
+//    private ArticleRepository articleRepository;
+//
+//    // GET
+//    @GetMapping("/api/articles")
+//    public List<Article> index() {
+//        return articleRepository.findAll();
+//    }
+//    @GetMapping("/api/articles/{id}")
+//    public Article show(@PathVariable Long id) {
+//        return articleRepository.findById(id).orElse(null);
+//    }
+//
+//    // POST
+//    @PostMapping("/api/articles")
+//    public Article create(@RequestBody ArticleForm dto) {
+//        Article article = dto.toEntity();
+//        return articleRepository.save(article);
+//    }
+//
+//    // PATCH
+//    @PatchMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+//        Article article = dto.toEntity();   // request body
+//        Article target = articleRepository.findById(id).orElse(null);   // url로 넘어온 id로 조회한 DB 데이터
+//
+//        if ((target == null) || (id != article.getId())) {
+//            log.info("Bad request! id: {}, article: {}", id, article.toString());
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        target.patch(article);
+//        Article updated = articleRepository.save(target);
+//        return ResponseEntity.status(HttpStatus.OK).body(updated);
+//    }
+//
+//    // DELETE
+//    @DeleteMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> delete(@PathVariable Long id) {
+//        Article target = articleRepository.findById(id).orElse(null);
+//        if (target == null) {
+//            log.info("Not exist! id: {}", id);
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        articleRepository.delete(target);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
+}
---
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -0,0 +1,87 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article show(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if (article.getId() != null) {
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        Article article = dto.toEntity();   // request body
+        Article target = articleRepository.findById(id).orElse(null);   // url로 넘어온 id로 조회한 DB 데이터
+
+        if ((target == null) || (id != article.getId())) {
+            log.info("Bad request! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+
+        target.patch(article);
+        return articleRepository.save(target);
+    }
+
+    public Article delete(Long id) {
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null) {
+            return null;
+        }
+
+        articleRepository.delete(target);
+        return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+
+//        List<Article> articleList = new ArrayList<>();
+//
+//        for (ArticleForm dto : dtos) {
+//            Article created = create(dto);
+//            if (created == null) {
+//                return null;
+//            }
+//            articleList.add(created);
+//        }
+
+        return articleList;
+    }
+}
